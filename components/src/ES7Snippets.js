5a4ddf74beee623cfc1bb00a0ec15b14b72efa9c
SoroushGReza <mycodeschool22@outlook.com>
2024-05-03 20:20:03 +0000

    Setup React Bootstrap and create Es7Snippets.js for reference during dev

---
+++ components/src/ES7Snippets.js
@@ -0,0 +1,67 @@
+// #1 SNiPPET  ----> rfce
+
+// import React from 'react'
+
+// function ES7Snippets() {
+//   return (
+//     <div>ES7Snippets</div>
+//   )
+// }
+
+// export default ES7Snippets
+
+//------------------------------------------------------------------
+
+// #2 SNiPPET  ----> rafce
+
+// import React from 'react'
+
+// const ES7Snippets = () => {
+//   return (
+//     <div>ES7Snippets</div>
+//   )
+// }
+
+// export default ES7Snippets
+
+//------------------------------------------------------------------
+
+// #3 SNiPPET  ----> rce
+
+// import React, { Component } from 'react'
+
+// export class ES7Snippets extends Component {
+//   render() {
+//     return (
+//       <div>ES7Snippets</div>
+//     )
+//   }
+// }
+
+// export default ES7Snippets
+
+//------------------------------------------------------------------
+
+// #4 SNiPPET  ----> rconst
+
+// constructor(props) {
+//   super(props)
+
+//   this.state = {
+//      first
+//   }
+// }
+
+//------------------------------------------------------------------
+
+// #5 SNiPPET  ----> clg
+
+// console.log(first)
+
+//------------------------------------------------------------------
+
+// #6 SNiPPET  ----> clo
+
+// console.log('first', first)
+
+//------------------------------------------------------------------
